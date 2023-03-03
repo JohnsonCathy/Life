738ee0bf19b9f33d94b0ab1025e83208fb1b3287
JohnsonCathy <124077081+JohnsonCathy@users.noreply.github.com>
2023-03-03 11:04:54 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 public class GUI extends JFrame {
     public static void main (String[] args) {
         JFrame board = new JFrame("Game of Life");
-        String start = JOptionPane.showInputDialog(board, "Would you like to go to college or start a career?");
+        int start = JOptionPane.showConfirmDialog(board, "Would you like to go to college?\n If not you'll start a career", null, JOptionPane.YES_NO_OPTION);
         Board gameBoard = new Board(start);
         ActionPanel display = new ActionPanel();
         board.add(gameBoard);
