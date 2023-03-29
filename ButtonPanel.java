c0416ff6536ddadf674655eaf7375399a6d7f06e
JohnsonCathy <124077081+JohnsonCathy@users.noreply.github.com>
2023-03-28 23:27:11 -0400

    Update ButtonPanel.java

--- ButtonPanel.java
+++ ButtonPanel.java
@@ -6,16 +6,17 @@ import java.util.Random;
 
 
 public class ButtonPanel extends JPanel {
-    private JButton spinButton = new JButton();
-    private JButton endTurnButton = new JButton("End turn");
-    private JButton buyStockButton = new JButton();
-    private JButton careers = new JButton(); //will be used in release 2
-    private JButton salaries = new JButton();
-    private JButton houses = new JButton();
+    public static JButton spinButton = new JButton();
+    public static JButton endTurnButton = new JButton("End turn");
+    public static JButton buyStockButton = new JButton();
+    public static JButton careers = new JButton(); //will be used in release 2
+    public static JButton salaries = new JButton();
+    public static JButton houses = new JButton();
     private boolean myTurn = true;
     private final Random rand = new Random();
     int upper = 6; //max number the spinner can generate
     int moves;
+    int card;
     private ImageIcon houseCardIcon;
     private ImageIcon salaryCardIcon;
     private ImageIcon careerCardIcon;
@@ -24,29 +25,37 @@ public class ButtonPanel extends JPanel {
     private Player player = new Player();
 
     public ButtonPanel() {
-        houseCardIcon = new ImageIcon("src/house_card.png");
-        salaryCardIcon = new ImageIcon("src/salary_card.png");
-        careerCardIcon = new ImageIcon("src/career_card.png");
-        stockCardIcon = new ImageIcon("src/stock_card.png");
-        spinnerIcon = new ImageIcon("src/spinner.jpg");
+        houseCardIcon = new ImageIcon("src/icons/house_card.png");
+        salaryCardIcon = new ImageIcon("src/icons/salary_card.png");
+        careerCardIcon = new ImageIcon("src/icons/career_card.png");
+        stockCardIcon = new ImageIcon("src/icons/stock_card.png");
+        spinnerIcon = new ImageIcon("src/icons/spinner.jpg");
 
         this.add(endTurnButton);
         endTurnButton.addActionListener(listener);
+
         spinButton.setIcon(spinnerIcon);
         this.add(spinButton);
         spinButton.addActionListener(listener);
+        ActionListener listener1 = new PlayerPanel();
+        spinButton.addActionListener(listener1);
+
         buyStockButton.setIcon(stockCardIcon);
         this.add(buyStockButton);
         buyStockButton.addActionListener(listener);
+
         careers.setIcon(careerCardIcon);
         this.add(careers);
         careers.addActionListener(listener);
+
         salaries.setIcon(salaryCardIcon);
         this.add(salaries);
         salaries.addActionListener(listener);
+
         houses.setIcon(houseCardIcon);
         this.add(houses);
         houses.addActionListener(listener);
+
         setLayout(new GridLayout(2, 3));
     }
 
@@ -55,13 +64,12 @@ public class ButtonPanel extends JPanel {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource().equals(spinButton)) {
-                if (myTurn == true) {
+                if (myTurn) {
                     moves = rand.nextInt(upper - 1) + 1; // +1 makes it so that player can not spin a value of 0
                     Board.movement(moves);
                     JOptionPane.showMessageDialog(null, "You spun a: " + moves);
                     myTurn = false;
-                }
-                else {
+                } else {
                     JOptionPane.showMessageDialog(null, "Slow down! You have already spun this turn. Please finish any remaining actions and press end turn.");
                 }
             }
@@ -73,28 +81,62 @@ public class ButtonPanel extends JPanel {
             }
             if (e.getSource().equals(careers)) {
                 Object[] options = {careerCardIcon, careerCardIcon, careerCardIcon, careerCardIcon, careerCardIcon};
-                int choice = JOptionPane.showOptionDialog(null, "Please draw a career card: ","Draw a card!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                int choice = JOptionPane.showOptionDialog(null, "Please draw a career card: ", "Draw a card!", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                String[] careerOptions = {"Doctor", "Teacher", "Pilot", "Police Officer", "Salesman"};
+                card = rand.nextInt(upper - 1) + 1;
+                String pick = careerOptions[card];
+                player.setCareer(pick);
             }
             if (e.getSource().equals(houses)) {
-                Object[] options = { houseCardIcon, houseCardIcon, houseCardIcon, houseCardIcon, houseCardIcon};
-                int choice = JOptionPane.showOptionDialog(null, "Please draw a house card: ","Draw a card!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
-                if(choice == JOptionPane.YES_OPTION || choice == JOptionPane.NO_OPTION || choice == JOptionPane.YES_NO_OPTION || choice == JOptionPane.CANCEL_OPTION){
-                    JOptionPane.showMessageDialog(null,"it works");
-                }
-                else{
-                    JOptionPane.showMessageDialog(null,"it kinda works");
+                Object[] options = {houseCardIcon, houseCardIcon, houseCardIcon, houseCardIcon, houseCardIcon};
+                int choice = JOptionPane.showOptionDialog(null, "Please draw a house card: ", "Draw a card!", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                if (choice == JOptionPane.YES_OPTION) {
+                    card = rand.nextInt(upper - 1) + 1;
+                    if (card == 1) {
+                        player.setHouse("Beach House");
+                    }
+                    if (card == 2) {
+                        player.setHouse("Mansion");
+                    }
+                    if (card == 3) {
+                        player.setHouse("Log Cabin");
+                    }
+                    if (card == 4) {
+                        player.setHouse("Trailer park");
+                    }
+                    if (card == 5) {
+                        player.setHouse("Apartment");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "it kinda works");
                 }
             }
             if (e.getSource().equals(salaries)) {
                 Object[] options = {salaryCardIcon, salaryCardIcon, salaryCardIcon, salaryCardIcon, salaryCardIcon};
-                int choice = JOptionPane.showOptionDialog(null, "Please draw a salary card: ","Draw a card!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                int choice = JOptionPane.showOptionDialog(null, "Please draw a salary card: ", "Draw a card!", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                if (choice == JOptionPane.YES_OPTION) {
+                    card = rand.nextInt(upper - 1) + 1;
+                    if (card == 1) {
+                        player.setSalary(20000);
+                    }
+                    if (card == 2) {
+                        player.setSalary(40000);
+                    }
+                    if (card == 3) {
+                        player.setSalary(60000);
+                    }
+                    if (card == 4) {
+                        player.setSalary(100000);
+                    }
+                    if (card == 5) {
+                        player.setSalary(150000);
+                    }
+                }
+                if (e.getSource().equals(buyStockButton)) {
+                    JOptionPane.showMessageDialog(null, "This button is useless, carry on.");
+                }
             }
-            if (e.getSource().equals(buyStockButton)) {
-                //Object[] options = { new JButton("one", houseCardIcon), new JButton("two", houseCardIcon), new JButton("three", houseCardIcon), new JButton("four", houseCardIcon), new JButton("five", houseCardIcon)};
-                JOptionPane.showMessageDialog(null,"This button is useless, carry on.");
-                //int choice = JOptionPane.showOptionDialog(null, "Please draw a house card: ","Draw a card!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
-            }
-            
         }
     };
 
+}
