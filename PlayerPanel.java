e23e9b5cfe6f95fc57f86eb38118dc26f5a0a614
JohnsonCathy <124077081+JohnsonCathy@users.noreply.github.com>
2023-03-28 23:25:36 -0400

    Update PlayerPanel.java

--- PlayerPanel.java
+++ PlayerPanel.java
@@ -2,42 +2,46 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
-public class PlayerPanel extends JPanel {
-
+public class PlayerPanel extends JPanel implements ActionListener {
     private Player playerOne = new Player();
     private Player playerTwo = new Player();
     private JLabel player1 = new JLabel("Player one profile");
     private JLabel player2 = new JLabel("Player two profile");
-    private JLabel money;
-    private JLabel career;
-    private JLabel salary;
-    private JLabel numTurns;
-    private JLabel currentTurn;
+    private static JLabel money1;
+    private static JLabel money2;
+    private static JLabel career;
+    private static JLabel salary;
+    private static JLabel numTurns;
+    private static JLabel currentTurn;
     private boolean whoTurn = playerOne.isCurrentPlayer();
+
 
     public PlayerPanel(){
         this.add(player1);
         numTurns = new JLabel(String.valueOf(playerOne.getRounds()));
         this.add(numTurns);
-        if (whoTurn == true) {
+        if (whoTurn) {
             currentTurn = new JLabel("Go player one");
         } else {
             currentTurn = new JLabel("Go player two");
         }
         this.add(currentTurn);
         this.add(player2);
+
         this.add(new JLabel("Money: "));
-        money = new JLabel(String.valueOf(playerOne.getMoney()));
-        this.add(money);
+        money1 = new JLabel(String.valueOf(playerOne.getMoney()));
+        this.add(money1);
+
         this.add(new JLabel("Money: "));
-        money = new JLabel(String.valueOf(playerTwo.getMoney()));
-        this.add(money);
+        money2 = new JLabel(String.valueOf(playerTwo.getMoney()));
+        this.add(money2);
+
         this.add(new JLabel("Career: "));
-        this.add(new JLabel("-void-"));
-        // this.add(career);
+        career = new JLabel(playerOne.getCareer());
+        this.add(career);
         this.add(new JLabel("Career: "));
-        this.add(new JLabel("-void-"));
-        // this.add(career);
+        career = new JLabel(playerTwo.getCareer());
+        this.add(career);
         this.add(new JLabel("Salary: "));
         this.add(new JLabel("-void-"));
         //  this.add(salary);
@@ -47,7 +51,19 @@ public class PlayerPanel extends JPanel {
 
         /*Grid size will need to be adjusted when label containing actual values are added
          * most likely will be 4,4 but im not sure yet*/
-
         setLayout(new GridLayout(4,4));
+
     }
+
+    public void actionPerformed(ActionEvent event) {
+        if(ButtonPanel.spinButton == event.getSource()) {
+            playerOne.setMoney(500);
+            money1.setText(String.valueOf(playerOne.getMoney()));
+        }
+        else if(event.getSource() == ButtonPanel.careers){
+            career.setText(playerOne.getCareer());
+        }
+
+    }
+
 }
