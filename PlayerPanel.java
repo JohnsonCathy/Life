30cb932a554ac73a17ce891ec6f047c8a130d404
BrandonMaz962 <122303241+BrandonMaz962@users.noreply.github.com>
2023-04-10 10:30:09 -0400

    Update PlayerPanel.java
    Complete overhaul. Labels now update with correct corresponding values when button clicked and also updates the correct player based on whos turn it is. Current player label also updates when turn ended.

--- PlayerPanel.java
+++ PlayerPanel.java
@@ -1,16 +1,24 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PlayerPanel extends JPanel implements ActionListener {
-    private Player playerOne = new Player();
-    private Player playerTwo = new Player();
+    private Player playerOne = ButtonPanel.player;
+    private Player playerTwo = ButtonPanel.player;
     private JLabel player1 = new JLabel("Player one profile");
     private JLabel player2 = new JLabel("Player two profile");
     private static JLabel money1;
     private static JLabel money2;
-    private static JLabel career;
-    private static JLabel salary;
+    private static JLabel career1;
+    private static JLabel career2;
+    private static JLabel salary1;
+    private static JLabel salary2;
+    private static JLabel house1;
+    private static JLabel house2;
+    private static JLabel ownedStock1;
+    private static JLabel ownedStock2;
     private static JLabel numTurns;
     private static JLabel currentTurn;
     private boolean whoTurn = playerOne.isCurrentPlayer();
@@ -18,13 +26,9 @@ public class PlayerPanel extends JPanel implements ActionListener {
 
     public PlayerPanel(){
         this.add(player1);
-        numTurns = new JLabel(String.valueOf(playerOne.getRounds()));
+        numTurns = new JLabel("Turn: " + playerOne.getRounds());
         this.add(numTurns);
-        if (whoTurn) {
-            currentTurn = new JLabel("Go player one");
-        } else {
-            currentTurn = new JLabel("Go player two");
-        }
+        currentTurn = new JLabel("Go player one!");
         this.add(currentTurn);
         this.add(player2);
 
@@ -37,33 +41,83 @@ public class PlayerPanel extends JPanel implements ActionListener {
         this.add(money2);
 
         this.add(new JLabel("Career: "));
-        career = new JLabel(playerOne.getCareer());
-        this.add(career);
+        career1 = new JLabel(playerOne.getCareer());
+        this.add(career1);
         this.add(new JLabel("Career: "));
-        career = new JLabel(playerTwo.getCareer());
-        this.add(career);
+        career2 = new JLabel(playerTwo.getCareer());
+        this.add(career2);
         this.add(new JLabel("Salary: "));
-        this.add(new JLabel("-void-"));
-        //  this.add(salary);
+        salary1 = new JLabel(String.valueOf(playerOne.getSalary()));
+        this.add(salary1);
         this.add(new JLabel("Salary: "));
-        this.add(new JLabel("-void-"));
-        //  this.add(salary);
+        salary2 = new JLabel(String.valueOf(playerTwo.getSalary()));
+        this.add(salary2);
+        this.add(new JLabel("House: "));
+        house1 = new JLabel(playerOne.getHouse());
+        this.add(house1);
+        this.add(new JLabel("House: "));
+        house2 = new JLabel(String.valueOf(playerTwo.getHouse()));
+        this.add(house2);
+        this.add(new JLabel("Stock: "));
+        ownedStock1 = new JLabel(String.valueOf(playerOne.getStock()));
+        this.add(ownedStock1);
+        this.add(new JLabel("Stock: "));
+        ownedStock2 = new JLabel(String.valueOf(playerTwo.getStock()));
+        this.add(ownedStock2);
+
 
         /*Grid size will need to be adjusted when label containing actual values are added
          * most likely will be 4,4 but im not sure yet*/
-        setLayout(new GridLayout(4,4));
+        setLayout(new GridLayout(6,4));
 
     }
 
     public void actionPerformed(ActionEvent event) {
         if(ButtonPanel.spinButton == event.getSource()) {
-            playerOne.setMoney(500);
-            money1.setText(String.valueOf(playerOne.getMoney()));
         }
         else if(event.getSource() == ButtonPanel.careers){
-            career.setText(playerOne.getCareer());
+            if(whoTurn == false) {
+                career1.setText(playerOne.getCareer());
+            } else if(whoTurn) {
+                career2.setText(playerTwo.getCareer());
+            }
         }
-
+        else if(ButtonPanel.endTurnButton == event.getSource()) {
+            playerOne.setRounds(playerOne.getRounds() + 1);
+            numTurns.setText("Turn: " + String.valueOf(playerOne.getRounds()));
+            if(playerOne.getRounds() % 2 == 1){
+                playerOne.isCurrentPlayer = false;
+            } else if(playerOne.getRounds() % 2 == 0) {
+                playerOne.setCurrentPlayer();
+            }
+            whoTurn = playerOne.isCurrentPlayer;
+            if (whoTurn == false) {
+                currentTurn.setText("Go player one!");
+            } else if (whoTurn){
+                currentTurn.setText("Go player two!");
+            }
+        }
+        else if(ButtonPanel.houses == event.getSource()) {
+            if(whoTurn == false) {
+                house1.setText(playerOne.getHouse());
+            } else if(whoTurn) {
+                house2.setText(playerTwo.getHouse());
+            }
+        }
+        else if(ButtonPanel.salaries == event.getSource()) {
+            if(whoTurn == false) {
+                salary1.setText(String.valueOf(playerOne.getSalary()));
+            } else if(whoTurn) {
+                salary2.setText(String.valueOf(playerTwo.getSalary()));
+            }
+        }
+        else if(ButtonPanel.buyStockButton == event.getSource()) {
+            if(whoTurn == false) {
+                ownedStock1.setText(String.valueOf(playerOne.getStock()));
+            } else if(whoTurn) {
+                ownedStock2.setText(String.valueOf(playerTwo.getStock()));
+            }
+        }
     }
 
 }
