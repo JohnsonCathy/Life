c798a439cab2b8b26b39eb0b1d7aa6d9fd47ce44
BrandonMaz962 <122303241+BrandonMaz962@users.noreply.github.com>
2023-04-10 10:26:12 -0400

    Update ButtonPanel.java
    Added functionality to stock button, and improved functions of all buttons.

--- ButtonPanel.java
+++ ButtonPanel.java
@@ -13,6 +13,7 @@ public class ButtonPanel extends JPanel {
     public static JButton salaries = new JButton();
     public static JButton houses = new JButton();
     private boolean myTurn = true;
+    private boolean haveSpun = false;
     private final Random rand = new Random();
     int upper = 6; //max number the spinner can generate
     int moves;
@@ -22,39 +23,44 @@ public class ButtonPanel extends JPanel {
     private ImageIcon careerCardIcon;
     private ImageIcon stockCardIcon;
     private ImageIcon spinnerIcon;
-    private Player player = new Player();
+    public static Player player = new Player();
 
     public ButtonPanel() {
-        houseCardIcon = new ImageIcon("src/icons/house_card.png");
-        salaryCardIcon = new ImageIcon("src/icons/salary_card.png");
-        careerCardIcon = new ImageIcon("src/icons/career_card.png");
-        stockCardIcon = new ImageIcon("src/icons/stock_card.png");
-        spinnerIcon = new ImageIcon("src/icons/spinner.jpg");
+        houseCardIcon = new ImageIcon("src/house_card.png");
+        salaryCardIcon = new ImageIcon("src/salary_card.png");
+        careerCardIcon = new ImageIcon("src/career_card.png");
+        stockCardIcon = new ImageIcon("src/stock_card.png");
+        spinnerIcon = new ImageIcon("src/spinner.jpg");
+        ActionListener listener1 = new PlayerPanel();
 
         this.add(endTurnButton);
         endTurnButton.addActionListener(listener);
+        endTurnButton.addActionListener(listener1);
 
         spinButton.setIcon(spinnerIcon);
         this.add(spinButton);
         spinButton.addActionListener(listener);
-        ActionListener listener1 = new PlayerPanel();
         spinButton.addActionListener(listener1);
 
         buyStockButton.setIcon(stockCardIcon);
         this.add(buyStockButton);
         buyStockButton.addActionListener(listener);
+        buyStockButton.addActionListener(listener1);
 
         careers.setIcon(careerCardIcon);
         this.add(careers);
         careers.addActionListener(listener);
+        careers.addActionListener(listener1);
 
         salaries.setIcon(salaryCardIcon);
         this.add(salaries);
         salaries.addActionListener(listener);
+        salaries.addActionListener(listener1);
 
         houses.setIcon(houseCardIcon);
         this.add(houses);
         houses.addActionListener(listener);
+        houses.addActionListener(listener1);
 
         setLayout(new GridLayout(2, 3));
     }
@@ -69,15 +75,21 @@ public class ButtonPanel extends JPanel {
                     Board.movement(moves);
                     JOptionPane.showMessageDialog(null, "You spun a: " + moves);
                     myTurn = false;
+                    haveSpun = true;
                 } else {
                     JOptionPane.showMessageDialog(null, "Slow down! You have already spun this turn. Please finish any remaining actions and press end turn.");
                 }
             }
 
             if (e.getSource().equals(endTurnButton)) {
-                JOptionPane.showMessageDialog(null, "Your turn has been ended, The next player may now move.");
-                myTurn = true;
-                player.setRounds(player.getRounds() + 1);
+                if (haveSpun) {
+                    JOptionPane.showMessageDialog(null, "Your turn has been ended, The next player may now move.");
+                    myTurn = true;
+                    haveSpun = false;
+                    player.hasStock = false;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Slow down! You have not spun this turn. Please finish any remaining actions and press spin.");
+                }
             }
             if (e.getSource().equals(careers)) {
                 Object[] options = {careerCardIcon, careerCardIcon, careerCardIcon, careerCardIcon, careerCardIcon};
@@ -107,8 +119,6 @@ public class ButtonPanel extends JPanel {
                     if (card == 5) {
                         player.setHouse("Apartment");
                     }
-                } else {
-                    JOptionPane.showMessageDialog(null, "it kinda works");
                 }
             }
             if (e.getSource().equals(salaries)) {
@@ -132,8 +142,39 @@ public class ButtonPanel extends JPanel {
                         player.setSalary(150000);
                     }
                 }
-                if (e.getSource().equals(buyStockButton)) {
-                    JOptionPane.showMessageDialog(null, "This button is useless, carry on.");
+            }
+            if (e.getSource().equals(buyStockButton)) {
+                Object[] options = {1, 2, 3, 4, 5};
+                int choice = JOptionPane.showOptionDialog(null, "Please select the stock you want to buy: ", "Draw a card!", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                if(player.hasStock() == false) {
+                    if (choice == 0) {
+                        int pick = 1;
+                        player.setStock(pick);
+                        player.setStock();
+                    }
+                    if (choice == 1) {
+                        int pick = 2;
+                        player.setStock(pick);
+                        player.setStock();
+                    }
+                    if (choice == 2) {
+                        int pick = 3;
+                        player.setStock(pick);
+                        player.setStock();
+                    }
+                    if (choice == 3) {
+                        int pick = 4;
+                        player.setStock(pick);
+                        player.setStock();
+                    }
+                    if (choice == 4) {
+                        int pick = 5;
+                        player.setStock(pick);
+                        player.setStock();
+                    }
+                }
+                    else if(player.hasStock() == true) {
+                    JOptionPane.showMessageDialog(null, "Hang on captain wall street, you already have a stock! You can only have one stock at a time please finish other actions then end your turn.");
                 }
             }
         }
