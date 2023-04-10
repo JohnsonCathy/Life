ae29065eb6bb4826e73fc76efe2be1429f5a0c22
BrandonMaz962 <122303241+BrandonMaz962@users.noreply.github.com>
2023-04-10 10:27:50 -0400

    Update Player.java
    Touched up getters and setters to accommodate other changes

--- Player.java
+++ Player.java
@@ -1,20 +1,42 @@
 public class Player {
     int money;
+    static int count;
     int rounds;
     int salary;
+    int ownedStock;
     String house = "";
     String career = "";
     boolean hasStock;
     boolean isCurrentPlayer;
+    public static Player PlayerOne;
+    private static Player PlayerTwo;
 
-    public Player(){
+    public Player() {
         money = 10000;
-        rounds = 0;
+        rounds = 1;
         hasStock = false;
-        house = "";
+        house = "Hobo";
         salary = 0;
-        career = "Broke ass";
+        career = "Bum";
+        ownedStock = 0;
+        isCurrentPlayer = false;
     }
+
+  /*  public static Player getInstance() {
+
+        switch (count) {
+            case 0:
+                PlayerOne = new Player();
+                count++;
+                return PlayerOne;
+            case 1:
+                PlayerTwo = new Player();
+                count++;
+                return PlayerTwo;
+            default:
+                return PlayerOne;
+        }
+    }*/
 
     /************GETTERS**************/
     public int getMoney(){
@@ -32,6 +54,7 @@ public class Player {
     public String getHouse() {return house;}
     public String getCareer() {return career;}
     public int getSalary() {return salary;}
+    public int getStock() {return ownedStock;}
 
     /************SETTERS**************/
     public void setMoney(int amount){
@@ -49,5 +72,6 @@ public class Player {
     public void setHouse(String house) {this.house = house;}
     public void setSalary(int salary) {this.salary = salary;}
     public void setCareer(String career) {this.career = career;}
+    public void setStock(int stock) {this.ownedStock = stock;}
 
 }
