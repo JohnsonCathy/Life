806914c56578eed16f32538bf179d221af97a09f
JohnsonCathy <124077081+JohnsonCathy@users.noreply.github.com>
2023-04-19 18:35:38 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,22 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyListener;
 
 public class GUI extends JFrame {
     public static void main (String[] args) {
         JFrame board = new JFrame("Game of Life");
-        //int start = JOptionPane.showConfirmDialog(board, "Would you like to go to college?\n If not you'll start a career","Choose a starting path!",JOptionPane.YES_NO_OPTION);
         Object[] options1 = { "College", "Career"};
-        int start = JOptionPane.showOptionDialog(board, "Would you like to go to college or start your career?","Choose a starting path!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options1, null);
-        int start2 =JOptionPane.showOptionDialog(board, "Player 2, would you like to go to college or start your career?","Choose a starting path!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options1, null);
-        Board gameBoard = new Board(start, start2);
+        int start = JOptionPane.showOptionDialog(board, "Player 1, would you like to go to college or start your career?","Choose a starting path!",JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options1, null);
+        Board gameBoard = new Board(start);
         ActionPanel display = new ActionPanel();
         board.add(gameBoard);
         board.add(display);
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        board.setDefaultCloseOperation(EXIT_ON_CLOSE);
         board.setLayout(new GridLayout(1,2));
         board.pack();
-        board.setLocationRelativeTo(null);
         board.setVisible(true);
     }
 }
